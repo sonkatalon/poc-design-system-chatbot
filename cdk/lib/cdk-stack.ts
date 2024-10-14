387c7e4285dc846969b9ec88196513aaf872f1ba
Dao Hoang Son <daohoangson@gmail.com>
2024-10-14 09:00:13 +0700

    Implement API to start ingestion job

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,74 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { bedrock } from "@cdklabs/generative-ai-cdk-constructs";
+import {
+  CfnOutput,
+  Duration,
+  RemovalPolicy,
+  Stack,
+  StackProps,
+} from "aws-cdk-lib";
+import * as apigw from "aws-cdk-lib/aws-apigateway";
+import * as iam from "aws-cdk-lib/aws-iam";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import { Construct } from "constructs";
+import { join } from "path";
 
-export class CdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class CdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const knowledgeBase = new bedrock.KnowledgeBase(this, "knowledgeBase", {
+      embeddingsModel: bedrock.BedrockFoundationModel.TITAN_EMBED_TEXT_V1,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const docsBucket = new s3.Bucket(this, "docsBucket", {
+      autoDeleteObjects: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    const s3DataSource = new bedrock.S3DataSource(this, "s3DataSource", {
+      bucket: docsBucket,
+      knowledgeBase: knowledgeBase,
+    });
+
+    const apiGateway = new apigw.RestApi(this, "restApi", {
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigw.Cors.ALL_ORIGINS,
+      },
+    });
+
+    const apiGatewayTimeout = Duration.seconds(29);
+    const startIngestionJobFunction = new NodejsFunction(
+      this,
+      "startIngestionJobFunction",
+      {
+        entry: join(__dirname, "../lambda/startIngestionJob/index.ts"),
+        environment: {
+          DATA_SOURCE_ID: s3DataSource.dataSourceId,
+          KNOWLEDGE_BASE_ID: knowledgeBase.knowledgeBaseId,
+        },
+        runtime: Runtime.NODEJS_20_X,
+        timeout: apiGatewayTimeout,
+      }
+    );
+    startIngestionJobFunction.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: ["bedrock:StartIngestionJob"],
+        resources: [knowledgeBase.knowledgeBaseArn, docsBucket.bucketArn],
+      })
+    );
+
+    apiGateway.root.addResource("ingestions").addMethod(
+      "POST",
+      new apigw.LambdaIntegration(startIngestionJobFunction, {
+        timeout: apiGatewayTimeout,
+      })
+    );
+
+    new CfnOutput(this, "ApiGatewayUrl", { value: apiGateway.url });
+
+    new CfnOutput(this, "DocsBucketName", { value: docsBucket.bucketName });
   }
 }
