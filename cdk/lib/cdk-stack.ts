4b561e8c7e38d7ef398ad2fad420401ced537d26
Dao Hoang Son <daohoangson@gmail.com>
2024-10-14 17:40:17 +0700

    Generate access key from CDK

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,5 +1,6 @@
 import { bedrock } from "@cdklabs/generative-ai-cdk-constructs";
 import { CfnOutput, RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import * as iam from "aws-cdk-lib/aws-iam";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 
@@ -22,6 +23,27 @@ export class CdkStack extends Stack {
       knowledgeBase: knowledgeBase,
     });
 
+    const user = new iam.User(this, "user");
+    const accessKey = new iam.AccessKey(this, "accessKey", { user });
+    const policy = new iam.Policy(this, "policy", {
+      statements: [
+        new iam.PolicyStatement({
+          actions: ["bedrock:Retrieve"],
+          resources: [knowledgeBase.knowledgeBaseArn],
+        }),
+        new iam.PolicyStatement({
+          actions: ["bedrock:InvokeModelWithResponseStream"],
+          resources: ["*"],
+        }),
+      ],
+    });
+    policy.attachToUser(user);
+
+    new CfnOutput(this, "AwsAccessKeyId", { value: accessKey.accessKeyId });
+    new CfnOutput(this, "AwsRegion", { value: this.region });
+    new CfnOutput(this, "AwsSecretAccessKey", {
+      value: accessKey.secretAccessKey.unsafeUnwrap(),
+    });
     new CfnOutput(this, "DocsBucketName", { value: docsBucket.bucketName });
     new CfnOutput(this, "KnowledgeBaseId", {
       value: knowledgeBase.knowledgeBaseId,
