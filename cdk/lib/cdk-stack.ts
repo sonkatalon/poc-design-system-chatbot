cd4a658c45946155ed2a5e2ae4b8e57bb7409ac0
Dao Hoang Son <daohoangson@gmail.com>
2024-10-19 20:33:25 +0700

    Deploy KB via `@cdklabs/generative-ai-cdk-constructs`

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,27 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { bedrock } from "@cdklabs/generative-ai-cdk-constructs";
+import { CfnOutput, RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import { Construct } from "constructs";
 
-export class CdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class CdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const knowledgeBase = new bedrock.KnowledgeBase(this, "knowledgeBase", {
+      embeddingsModel: bedrock.BedrockFoundationModel.TITAN_EMBED_TEXT_V1,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const docsBucket = new s3.Bucket(this, "docsBucket", {
+      autoDeleteObjects: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    const s3DataSource = new bedrock.S3DataSource(this, "s3DataSource", {
+      bucket: docsBucket,
+      knowledgeBase: knowledgeBase,
+    });
+
+    new CfnOutput(this, "DocsBucketName", { value: docsBucket.bucketName });
   }
 }
